5beed078308208aa6e5620e5e7fb0a6661413094
Sumiet Gore <sumeetgore96@gmail.com>
2023-08-04 17:29:19 +0200

    modified:   app/api/auth/[...nextauth]/route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 
-export const runtime = 'edge';
-
 const handler = NextAuth({
     providers: [
         Credentials({
