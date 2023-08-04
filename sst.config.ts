c8ee8905133b05748b6c5f5c232bbcab7d0748e5
Sumiet Gore <sumeetgore96@gmail.com>
2023-08-05 00:51:09 +0200

    new file:   .env

---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "my-app",
+      region: "ap-south-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site");
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
